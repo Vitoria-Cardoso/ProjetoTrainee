6a7462f1a99ac3653eb8f9a39209692c323fcc19
Vitoria-Cardoso <67843070+Vitoria-Cardoso@users.noreply.github.com>
2022-06-14 14:56:27 -0300

    new task janela css js

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ function openFrase() {
 function closeFrase() {
   document.body.classList.remove('frases-expanded')
 }
+
+
+//funttion abrir new-task
+function openNewTask() {
+  document.body.classList.add('new-task-expanded')
+  
+}
+
+//funttion fechar new-task
+function closeNewTask() {
+  document.body.classList.remove('new-task-expanded')
+  
+}
