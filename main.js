912a9f21710a42c06957ad42c58a4b83ef1fb84e
Vitoria-Cardoso <67843070+Vitoria-Cardoso@users.noreply.github.com>
2022-06-13 22:29:18 -0300

    botao frases do dia modal JS

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//funcao para abrir frase
+function openFrase() {
+  document.body.classList.add('frases-expanded')
+  
+}
+
+//function fechar frase
+function closeFrase() {
+  document.body.classList.remove('frases-expanded')
+}
