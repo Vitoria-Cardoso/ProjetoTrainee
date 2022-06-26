173b7738dbb1f0e19cc8ffc62b8533c2e9ddd749
Vitoria-Cardoso <67843070+Vitoria-Cardoso@users.noreply.github.com>
2022-06-26 15:53:50 -0300

    achar task values

--- task.js
+++ task.js
@@ -3,12 +3,14 @@ let taskAFazer = [] //? A fazer
 let taskFazendo = [] //? Fazendo
 let taskFeito = [] //? Feito
 
-//* form
-document.getElementById("newTaskForm").addEventListener("submit", (event) => {
+//! form
+document.getElementById('newTaskForm').addEventListener('submit', event => {
   event.preventDefault()
   criarTask() //? function
-  document.getElementById("modalNewTask").style.display = "none"
+  document.getElementById('modalNewTask').style.display = 'none'
 })
+
+
 
 //! FUNCITION ADD TASK A FAZER
 //* Primeira funcao chamada quando user submeter o form
@@ -16,33 +18,37 @@ function criarTask() {
   const dados = getDados() //? function pegar os dados - obj
   taskAFazer.push(dados) //? add os dados no array de obj
 
-  //? limpa o UL pra dps repopular 
-  const ul = document.getElementById("listAFazer")
-  ul.innerHTML = "" //? limpa ul 
+  //? limpa o UL pra dps repopular
+  const ul = document.getElementById('listAFazer')
+  ul.innerHTML = '' //? limpa ul
 
   //! --
   //? visita cada obj do array taskAFazer e manda pra function popularLista()
-  taskAFazer.forEach((task) => {
+  taskAFazer.forEach(task => {
     //? function
-    popularLista("listAFazer", task.titulo, task.descricao)
+    popularLista('listAFazer', task.titulo, task.descricao)
   })
+
+  //achar task para mover 
+  acharTask()
 }
+
 
 //* Function que pega os dados do submit e retorna obj
 function getDados() {
-  const inputTitulo = document.getElementById("inpTituloTask") 
+  const inputTitulo = document.getElementById('inpTituloTask')
   const titulo = inputTitulo.value
-  
-  const inputDescricao = document.getElementById("inpDescTask")
+
+  const inputDescricao = document.getElementById('inpDescTask')
   const descricao = inputDescricao.value
 
+
   //!mensagem de erro
-  if (titulo && descricao != "") {
-    return {titulo, descricao}
-  }else {
-    alert("Acho que voce esqueceu de preencher algum campo ⚠️")
+  if (titulo && descricao != '') {
+    return { titulo, descricao }
+  } else {
+    alert('Acho que voce esqueceu de preencher algum campo ⚠️')
   }
-  
 }
 
 //* Function de popular as listas, injetar o html
@@ -52,29 +58,62 @@ function popularLista(lista, titulo, descricao) {
 
   //?injeta o elemtento
   ul.appendChild(li)
+
+  //*mover
+  document.getElementById('insertTitle').value = titulo
+  document.getElementById('insertDescricao').value = descricao
 }
 
 //* Function cria o html da li e add a class task pra style dps
 function criarTaskElement(titulo, descricao) {
-  const li = document.createElement("li") //? cria li
-  li.classList.add("task") //? add class pra css dps
+  const li = document.createElement('li') //? cria li
+  li.classList.add('task') //? add class pra css dps
 
   //? HTML da li , injetado pela popularLista()
-  li.innerHTML =  `
-  <div class = "container-li"> 
-  <div class="titulo-li" > <p> ${titulo} </p> <span class="material-symbols-outlined">
-  more_vert
-  </span>
-  </div>
-  <div class = "desc-li" >
-  <p> ${descricao} </p> <button class="navigate-icon" > <span class="material-icons-outlined">
-  navigate_next
-  </span></button>
-  </div>
-  </div>
+  li.innerHTML = `
+  <form class="container-li" id="mudarTask">
+      <label class="titulo-li">
+          <p>${titulo}</p>
+          <span class="material-symbols-outlined">
+            more_vert
+          </span>
+			</label>
+			<input type="text" id="insertTitle">
+
+      <div class="desc-li">
+				<label>
+				<p>${descricao}</p>
+				</label>
+				<input type="text" id="insertDescricao">
+        <input class="navigate-icon material-icons-outlined" type="submit" value="navigate_next">
+      </div>
+    </form>
   `
   return li
 }
 
-//! ADICIONAR TASK NO AFAZER TA FUNCIONANDO  
+//! ADICIONAR TASK NO AFAZER TA FUNCIONANDO
+
+
+//!mover task
+
+function acharTask(){
+  document.getElementById('mudarTask').addEventListener('submit', (event) => {
+    event.preventDefault()
+    getDadosTask()
+  })
+}
+
+
+//!PEGA os dados da task que quer mover e manda pra moverTask()
+function getDadosTask() {
+  const inputTitulo = document.getElementById('insertTitle')
+  const titulo = inputTitulo.value
+
+  const inputDescricao = document.getElementById('insertDescricao')
+  const descricao = inputDescricao.value
+
+  return { titulo, descricao }
+}
+
 
